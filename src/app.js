53554c612cbb81b1070bb2489f521acbd74656dd
Raza Ghulam <ahmad.abdali001@gmail.com>
2020-07-30 12:56:18 -0700

    Changes to be committed:

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    title: "Help will be provided soon!",
+    title: "Coming soon!",
     name: "Raza Ghulam"
   })
 })
