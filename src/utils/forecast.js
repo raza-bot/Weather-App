c637f9b5cdbb20c5f247960db215cdcfb0a65a84
Raza Ghulam <ahmad.abdali001@gmail.com>
2020-07-30 12:28:36 -0700

    Weather units changed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=cd2a6ffea01ac28c3a4db159578421c3"
 
 const forecast = (lat, lon, callback) => {
-  const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=us&appid=cd2a6ffea01ac28c3a4db159578421c3'
+  const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric&appid=cd2a6ffea01ac28c3a4db159578421c3'
   request({url, json: true}, (error, {body}) => { 
     if (error) { 
       callback("Unable to find the weather servive API!!", undefined)
